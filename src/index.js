92ca8646447d16322db7cc789800a0323ec5e5ec
Alisher Usmonov <83527629+Alisher-Usmonov@users.noreply.github.com>
2023-01-14 11:47:40 +0500

    feat: first commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const TelegramBot = require("node-telegram-bot-api");
+const { TOKEN } = require("../config");
+const axios = require("axios").default;
+
+const bot = new TelegramBot(TOKEN, {
+  polling: true,
+});
+
+bot.on("message", async (msg) => {
+  const chatId = msg.chat.id;
+  const text = msg.text;
+  try {
+    if (text == "/start") {
+      bot.sendMessage(chatId, "Assalamu alaikum dastuchi");
+    } else {
+      const { data } = await axios.get(
+        `https://api.stackexchange.com/2.2/search?order=desc&sort=votes&site=stackoverflow&intitle=${text}`
+      );
+      const answers = data.items;
+      if (answers.length > 0) {
+        const answerLinks = answers
+          .map((answer) => `<a href="${answer.link}">${answer.title}</a>`)
+          .join("\n\n");
+        bot.sendMessage(chatId, `<b>Javoblar:</b>\n\n${answerLinks}`, {
+          parse_mode: "HTML",
+        });
+      } else {
+        bot.sendMessage(chatId, "Javob topilmadi");
+      }
+    }
+  } catch (error) {
+    console.error(error);
+  }
+});
